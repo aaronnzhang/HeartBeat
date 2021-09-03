51d3b288b7a5da2135b00ec3e7611408cdb4df0a
aaronnzhang <zhangyihao319@gmail.com>
2021-09-03 09:59:46 +0800

    [test] add test should return 200 when request is valid

--- backend/tests/controller/GenerateReportController.test.ts
+++ backend/tests/controller/GenerateReportController.test.ts
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 chai.should();
 
 describe("GenerateReporter", () => {
-  it("should return 200 and report data when post data correct", async () => {
+  it.skip("should return 200 and report data when post data correct", async () => {
     const response = await chai
       .request(app)
       .post("/generateReporter")
@@ -24,4 +24,32 @@ describe("GenerateReporter", () => {
     const response = await chai.request(app).post("/generateReporter").send({});
     expect(response.status).equal(400);
   });
+
+  it("should return 200 when request is valid", async () => {
+    const mockData = {
+      "metrics": ["Lead time for changes"],
+      "startTime": 1630252800000,
+      "endTime": 1630511999000,
+      "considerHoliday": false,
+      "pipeline": {"type": "BuildKite", "token": "9627ef784bd5dac24256bc7d382d6af2ce77a781", "deployment": []},
+      "codebaseSetting": {
+        "type": "Github",
+        "token": "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",
+        "leadTime": [{
+          "orgId": "aaa-11",
+          "orgName": "aaa",
+          "id": "firstpipeline",
+          "name": "firstPipeline",
+          "step": "test",
+          "repository": "git@github.com:gtb-203b-xin-ye/B-basic-quiz.git"
+        }]
+      },
+      "csvTimeStamp": 1630566152884
+    };
+    const response = await chai
+        .request(app)
+        .post("/generateReporter")
+        .send(mockData);
+    expect(response.status).to.equal(200);
+  }).timeout(10000);
 });
